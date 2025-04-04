ab51e0c83e4f5100f33b15712b2f2d4b39b3185e
Thành Zanh <114275350+thanhzanh@users.noreply.github.com>
2025-04-05 01:22:08 +0700

    fix lan 4

--- config/db.js
+++ config/db.js
@@ -7,10 +7,13 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT,      
   waitForConnections: true, 
   connectionLimit: 10,      
   queueLimit: 0,
-  port: process.env.DB_PORT,      
+  ssl: { 
+    rejectUnauthorized: false  // Có thể cần thiết cho kết nối an toàn qua public network
+  }
 });
 
 // Kiểm tra kết nối
